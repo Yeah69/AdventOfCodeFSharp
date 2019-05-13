739832e120a1ba2d8e96297e012f2cfce98c4baa
Dimitri Enns <dimitri.enns@taktiq.de>
2019-05-13 10:32:07 +0200

    Preparations for 2016

--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -76,6 +76,32 @@ let main _ =
     | Year 2015, Day 23 -> trackDayPerformance(Year2015.Day23.go)
     | Year 2015, Day 24 -> trackDayPerformance(Year2015.Day24.go)
     | Year 2015, Day 25 -> trackDayPerformance(Year2015.Day25.go)
+    
+    | Year 2016, Day 1 -> trackDayPerformance(Year2016.Day1.go)
+    | Year 2016, Day 2 -> trackDayPerformance(Year2016.Day2.go)
+    | Year 2016, Day 3 -> trackDayPerformance(Year2016.Day3.go)
+    | Year 2016, Day 4 -> trackDayPerformance(Year2016.Day4.go)
+    | Year 2016, Day 5 -> trackDayPerformance(Year2016.Day5.go)
+    | Year 2016, Day 6 -> trackDayPerformance(Year2016.Day6.go)
+    | Year 2016, Day 7 -> trackDayPerformance(Year2016.Day7.go)
+    | Year 2016, Day 8 -> trackDayPerformance(Year2016.Day8.go)
+    | Year 2016, Day 9 -> trackDayPerformance(Year2016.Day9.go)
+    | Year 2016, Day 10 -> trackDayPerformance(Year2016.Day10.go)
+    | Year 2016, Day 11 -> trackDayPerformance(Year2016.Day11.go)
+    | Year 2016, Day 12 -> trackDayPerformance(Year2016.Day12.go)
+    | Year 2016, Day 13 -> trackDayPerformance(Year2016.Day13.go)
+    | Year 2016, Day 14 -> trackDayPerformance(Year2016.Day14.go)
+    | Year 2016, Day 15 -> trackDayPerformance(Year2016.Day15.go)
+    | Year 2016, Day 16 -> trackDayPerformance(Year2016.Day16.go)
+    | Year 2016, Day 17 -> trackDayPerformance(Year2016.Day17.go)
+    | Year 2016, Day 18 -> trackDayPerformance(Year2016.Day18.go)
+    | Year 2016, Day 19 -> trackDayPerformance(Year2016.Day19.go)
+    | Year 2016, Day 20 -> trackDayPerformance(Year2016.Day20.go)
+    | Year 2016, Day 21 -> trackDayPerformance(Year2016.Day21.go)
+    | Year 2016, Day 22 -> trackDayPerformance(Year2016.Day22.go)
+    | Year 2016, Day 23 -> trackDayPerformance(Year2016.Day23.go)
+    | Year 2016, Day 24 -> trackDayPerformance(Year2016.Day24.go)
+    | Year 2016, Day 25 -> trackDayPerformance(Year2016.Day25.go)
 
     | Year 2018, Day 1 -> trackDayPerformance(Year2018.Day1.go)
     | Year 2018, Day 2 -> trackDayPerformance(Year2018.Day2.go)
@@ -102,6 +128,7 @@ let main _ =
     | Year 2018, Day 23 -> trackDayPerformance(Year2018.Day23.go)
     | Year 2018, Day 24 -> trackDayPerformance(Year2018.Day24.go)
     | Year 2018, Day 25 -> trackDayPerformance(Year2018.Day25.go)
+
     | _ -> "" |> ignore
     
     
---
+++ AdventOfCode/Year2016.fs
@@ -0,0 +1,254 @@
+﻿module Year2016
+
+open Domain
+open Operations
+
+module Day1 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.01.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day2 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.02.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day3 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.03.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day4 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.04.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day5 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.05.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day6 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.06.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day7 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.07.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day8 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.08.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day9 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.09.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day10 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.10.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day11 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.11.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day12 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.12.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day13 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.13.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day14 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.14.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day15 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.15.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day16 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.16.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day17 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.17.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day18 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.18.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day19 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.19.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day20 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.20.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day21 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.21.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day22 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.22.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day23 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.23.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day24 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.24.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
+
+module Day25 =
+    let go() =
+        let input = inputFromResource "AdventOfCode.Inputs._2016.25.txt"
+
+        let result1 = 0
+
+        let result2 = 0
+
+        { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
