dea5030e1d4855d454d6e5e2cd4e3b9f9525e06c
Dima Enns <dieter.enns@gmail.com>
2019-05-16 22:03:49 +0200

    2017 Day 1

--- AdventOfCode/Year2017.fs
+++ AdventOfCode/Year2017.fs
@@ -4,12 +4,31 @@ open Domain
 open Operations
 
 module Day1 =
+    let calculateCaptcha (input:string) =
+        input
+        |> Seq.append (seq { yield (input |> Seq.last) })
+        |> Seq.pairwise
+        |> Seq.filter (fun (c1, c2) -> c1 = c2)
+        |> Seq.map fst
+        |> Seq.map (fun c -> Integer.Parse (sprintf "%c" c))
+        |> Seq.sum
+
+    let calculateNextCaptcha (input:string) =
+        let half = input.Length / 2
+        let firstArray = input |> Seq.take half |> Seq.toArray
+        let secondArray = input |> Seq.skip half |> Seq.toArray
+        firstArray 
+        |> Seq.mapi (fun i c -> i,c) 
+        |> Seq.filter (fun (i, c) -> c = ((secondArray, i) ||> Array.get))
+        |> Seq.map (fun (_, c) -> Integer.Parse (sprintf "%c" c) * 2)
+        |> Seq.sum
+    
     let go() =
         let input = inputFromResource "AdventOfCode.Inputs._2017.01.txt"
 
-        let result1 = 0
+        let result1 = input |> calculateCaptcha
 
-        let result2 = 0
+        let result2 = input |> calculateNextCaptcha
 
         { First = sprintf "%d" result1; Second = sprintf "%d" result2 }
 
